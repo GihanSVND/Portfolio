807b0352d47ee32b35467af4199cc392175b3e00
GihanSVND <gihansvnd@gmail.com>
2024-09-26 21:58:48 +0530

    make responsive the time line

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
             alt="File icon"
             width={600}
             height={600}
-            className="z-10 transform scale-100 -translate-y-[-100px] transition-transform duration-500 ease-out sm:w-[600px] sm:h-[600px] md:w-[600px] md:h-[600px] lg:w-[600px] lg:h-[600px]"
+            className="z-10 transform scale-100 -translate-y-[-100px] transition-transform duration-100 ease-out sm:w-[600px] sm:h-[600px] md:w-[600px] md:h-[600px] lg:w-[600px] lg:h-[600px]"
           />
 
           {/* Large Text */}
@@ -115,131 +115,118 @@ export default function Home() {
 
         <div className="flex flex-col translate-y-[-120px] justify-center items-center relative ">
           <h1
-            className={`${poppins5.className} absolute text-[200px] font-extrabold text-gray-300`}
+            className={`${poppins5.className} absolute text-[40px]   // xs
+      sm:text-[60px]
+      md:text-[100px]
+      lg:text-[150px]
+      xl:text-[200px]
+      2xl:text-[250px] font-extrabold text-gray-300`}
           >
             Time Line
             <span className="text-[#FF4401]">.</span>
           </h1>
         </div>
         <div>
-          <div className="relative flex flex-col items-center py-[100px]">
-            {/* Vertical Line */}
-            <div className="absolute w-[1px] bg-[#FF4401] h-[700px] left-1/2 transform -translate-x-1/2"></div>
-            <div className="absolute bottom-16 w-[50px] border-t-[1px] border-[#FF4401]  translate-y-[17px]"></div>
 
-            {/* Timeline Cards */}
-            <div className="space-y-[100px]">
-              {/* First card: Logo Designer */}
-              <div className="flex items-center w-full">
-                <div className="absolute w-4 h-4 bg-[#FF4401] rounded-full left-1/2 transform -translate-x-1/2"></div>
-                <div className="flex justify-start pr-10 translate-x-[-300px]">
-                  <div className="bg-[#18181b] border-[1px] border-white text-white p-8 w-[400px] h-[150px]  rounded-[30px] shadow-lg">
-                    <div className="flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="size-20 pr-5"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M2.25 5.25a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3V15a3 3 0 0 1-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 0 1-.53 1.28h-9a.75.75 0 0 1-.53-1.28l.621-.622a2.25 2.25 0 0 0 .659-1.59V18h-3a3 3 0 0 1-3-3V5.25Zm1.5 0v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5Z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
+  <div className="relative flex flex-col items-center py-[100px]">
+    {/* Vertical Line */}
+    <div className="z-10 absolute w-[1px] bg-[#FF4401] h-[700px] left-1/2 transform -translate-x-1/2"></div>
+    <div className="absolute bottom-16 w-[50px] border-t-[1px] border-[#FF4401] translate-y-[17px]"></div>
 
-                      <div>
-                        <h3
-                          className={`${poppins7.className} text-[30px] font-bold text-[#FF4401]`}
-                        >
-                          Co-Founder
-                        </h3>
-                        <p className={`${poppins4.className} text-sm`}>GenO3</p>
-                        <p className={`${poppins4.className} text-sm`}>
-                          Apr 2024 – Present
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-1/2"></div> {/* Empty space for spacing */}
-              </div>
-
-              {/* Second card: 3D Artist */}
-              <div className="flex items-center w-full">
-                <div className="absolute w-4 h-4 bg-[#FF4401] rounded-full left-1/2 transform -translate-x-1/2"></div>
-                <div className="w-1/2"></div> {/* Empty space for spacing */}
-                <div className="flex justify-start pr-10 translate-x-[350px]">
-                  <div className="bg-[#18181b] border-[1px] border-white text-white p-8 w-[400px] h-[150px]  rounded-[30px] shadow-lg">
-                    <div className="flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="size-20 pr-5"
-                      >
-                        <path d="M12.378 1.602a.75.75 0 0 0-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03ZM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 0 0 .372-.648V7.93ZM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 0 0 .372.648l8.628 5.033Z" />
-                      </svg>
-
-                      <div>
-                        <h3
-                          className={`${poppins7.className} text-[30px] font-bold text-[#FF4401]`}
-                        >
-                          3D Artist
-                        </h3>
-                        <p className={`${poppins4.className} text-sm`}>
-                          {" "}
-                          CrewGen - Full time{" "}
-                        </p>
-                        <p className={`${poppins4.className} text-sm`}>
-                          Jan 2021 – Dec 2022
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              {/* Third card: Co-Founder */}
-              <div className="flex items-center w-full">
-                <div className="absolute w-4 h-4 bg-[#FF4401] rounded-full left-1/2 transform -translate-x-1/2"></div>
-                <div className="flex justify-start pr-10 translate-x-[-300px]">
-                  <div className="bg-[#18181b] border-[1px] border-white text-white p-8 w-[400px] h-[150px]  rounded-[30px] shadow-lg">
-                    <div className="flex items-center">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 24 24"
-                        fill="currentColor"
-                        className="size-20 pr-5"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-
-                      <div>
-                        <h3
-                          className={`${poppins7.className} text-[30px] font-bold text-[#FF4401]`}
-                        >
-                          Logo Designer
-                        </h3>
-                        <p className={`${poppins4.className} text-sm`}>
-                          CrewGen - Full time
-                        </p>
-                        <p className={`${poppins4.className} text-sm`}>
-                          Jan 2020 – Dec 2020
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="w-1/2"></div> {/* Empty space for spacing */}
+    {/* Timeline Cards */}
+    <div className="z-40 space-y-[100px]">
+      {/* First card: Logo Designer */}
+      <div className="flex items-center w-full">
+        <div className="absolute w-4 h-4 bg-[#FF4401] rounded-full left-1/2 transform translate-y-[75px] -translate-x-1/2"></div>
+        <div className="flex justify-center sm:justify-start sm:pr-10 sm:translate-x-[-300px]">
+          <div className="bg-[#18181b] border-[1px] border-white text-white p-8 w-[300px] sm:w-[400px] h-[150px] rounded-[30px] shadow-lg">
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-20 pr-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M2.25 5.25a3 3 0 0 1 3-3h13.5a3 3 0 0 1 3 3V15a3 3 0 0 1-3 3h-3v.257c0 .597.237 1.17.659 1.591l.621.622a.75.75 0 0 1-.53 1.28h-9a.75.75 0 0 1-.53-1.28l.621-.622a2.25 2.25 0 0 0 .659-1.59V18h-3a3 3 0 0 1-3-3V5.25Zm1.5 0v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <div>
+                <h3 className={`${poppins7.className} text-[20px] sm:text-[30px] font-bold text-[#FF4401]`}>
+                  Co-Founder
+                </h3>
+                <p className={`${poppins4.className} text-sm`}>GenO3</p>
+                <p className={`${poppins4.className} text-sm`}>Apr 2024 – Present</p>
               </div>
             </div>
           </div>
         </div>
+        <div className="hidden sm:block w-1/2"></div> {/* Empty space for larger screens */}
+      </div>
+
+      {/* Second card: 3D Artist */}
+      <div className="flex items-center w-full">
+      <div className="absolute w-4 h-4 bg-[#FF4401] rounded-full left-1/2 transform translate-y-[75px] -translate-x-1/2"></div>
+        <div className="hidden sm:block w-1/2"></div> {/* Empty space for larger screens */}
+        <div className="flex justify-center sm:justify-start sm:pr-10 sm:translate-x-[350px]">
+          <div className="bg-[#18181b] border-[1px] border-white text-white p-8 w-[300px] sm:w-[400px] h-[150px] rounded-[30px] shadow-lg">
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-20 pr-5"
+              >
+                <path d="M12.378 1.602a.75.75 0 0 0-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03ZM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 0 0 .372-.648V7.93ZM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 0 0 .372.648l8.628 5.033Z" />
+              </svg>
+              <div>
+                <h3 className={`${poppins7.className} text-[20px] sm:text-[30px] font-bold text-[#FF4401]`}>
+                  3D Artist
+                </h3>
+                <p className={`${poppins4.className} text-sm`}>CrewGen - Full time</p>
+                <p className={`${poppins4.className} text-sm`}>Jan 2021 – Dec 2022</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Third card: Logo Designer */}
+      <div className="flex items-center w-full">
+      <div className="absolute w-4 h-4 bg-[#FF4401] rounded-full left-1/2 transform translate-y-[75px] -translate-x-1/2"></div>
+        <div className="flex justify-center sm:justify-start sm:pr-10 sm:translate-x-[-300px]">
+          <div className="bg-[#18181b] border-[1px] border-white text-white p-8 w-[300px] sm:w-[400px] h-[150px] rounded-[30px] shadow-lg">
+            <div className="flex items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-20 pr-5"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M1.5 6a2.25 2.25 0 0 1 2.25-2.25h16.5A2.25 2.25 0 0 1 22.5 6v12a2.25 2.25 0 0 1-2.25 2.25H3.75A2.25 2.25 0 0 1 1.5 18V6ZM3 16.06V18c0 .414.336.75.75.75h16.5A.75.75 0 0 0 21 18v-1.94l-2.69-2.689a1.5 1.5 0 0 0-2.12 0l-.88.879.97.97a.75.75 0 1 1-1.06 1.06l-5.16-5.159a1.5 1.5 0 0 0-2.12 0L3 16.061Zm10.125-7.81a1.125 1.125 0 1 1 2.25 0 1.125 1.125 0 0 1-2.25 0Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <div>
+                <h3 className={`${poppins7.className} text-[20px] sm:text-[30px] font-bold text-[#FF4401]`}>
+                  Logo Designer
+                </h3>
+                <p className={`${poppins4.className} text-sm`}>CrewGen - Full time</p>
+                <p className={`${poppins4.className} text-sm`}>Jan 2020 – Dec 2020</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="hidden sm:block w-1/2"></div> {/* Empty space for larger screens */}
+      </div>
+    </div>
+  </div>
+</div>
+
 
         <div className="flex flex-col py-[200px] justify-center items-center relative ">
           <h1
