33ba3ca51eac8dc134ead86aa7663b6f1a251192
Gihan Savinda <gihansvnd@gmail.com>
2025-05-12 23:47:16 +0530

    fix dep errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
 "use client";
 
-import { Bokor, Poppins } from "next/font/google";
-
-import { useState } from "react";
-import React, { useRef, FormEvent } from "react";
-import emailjs from "@emailjs/browser";
 
 import localFont from "next/font/local";
 
@@ -16,41 +11,7 @@ const kugileFont = localFont({
 });
 
 
-// Define your categories
-const categories = ["UI/UX", "Swift", "Web", "ML"];
-
 export default function Home() {
-  const form = useRef<HTMLFormElement | null>(null);
-
-  const sendEmail = (e: FormEvent) => {
-    e.preventDefault();
-
-    if (form.current) {
-      emailjs
-        .sendForm("service_ogkst05", "template_t9urzgm", form.current, {
-          publicKey: "mH9V7Z_Kh9KMP8dn5",
-        })
-        .then(
-          () => {
-            console.log("SUCCESS!");
-          },
-          (error) => {
-            console.log("FAILED...", error.text);
-          }
-        );
-    }
-  };
-
-  const [selectedCategory, setSelectedCategory] = useState<string>(
-    categories[0]
-  );
-
-  // Handle category change
-  const handleCategoryChange = (category: string) => {
-    setSelectedCategory(category);
-    // Add logic to fetch/filter/display projects based on category
-    console.log("Selected category:", category);
-  };
 
   return (
     <main>
