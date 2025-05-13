6a6d44e3bd775e4c0cb3c3a230b1cccd5c5bf509
Gihan Savinda <gihansvnd@gmail.com>
2025-05-13 12:21:07 +0530

    New sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import CollectionSection from "@/components/CollectionSection";
 
 import localFont from "next/font/local";
 
@@ -15,6 +16,8 @@ export default function Home() {
     <main>
       <div className="bg-[#ECE8D5] flex flex-col items-center justify-center ">
         <ThinkerSection kugileFont={kugileFont} />
+        // Example usage
+        
         <Image
           src={"/book.png"}
           alt="Thinker portrait in ornate gold frame"
@@ -23,7 +26,36 @@ export default function Home() {
           className="max-w-full h-auto transition-opacity duration-300"
           priority
         />
-        <h1 className={`${kugileFont.className} text-4xl text-[#2a0e04] md:text-5xl mb-4`}> - His Journey - </h1>
+        <CollectionSection
+          kugileFont={kugileFont}
+          items={[
+            {
+              id: 1,
+              image: "/col1.png",
+              caption: "Socs",
+              url: "/your-link-destination",
+            },
+            {
+              id: 2,
+              image: "/col1.png",
+              caption: "Your Custom Caption",
+              url: "/your-link-destination",
+            },
+            {
+              id: 3,
+              image: "/col1.png",
+              caption: "s",
+              url: "/your-link-destination",
+            },
+            // Add more items as needed
+          ]}
+        />
+        <h1
+          className={`${kugileFont.className} text-4xl text-[#2a0e04] md:text-5xl mb-4`}
+        >
+          {" "}
+          - His Journey -{" "}
+        </h1>
         <Image
           src={"/map.png"}
           alt="Thinker portrait in ornate gold frame"
